431788449741ef4544cbbb0de634805cb978fb2c
FlaSh <nexus2493@gmail.com>
2020-12-08 23:51:04 +0900

    socketIO indexjs 연결

--- index.js
+++ index.js
@@ -9,11 +9,13 @@ import dotenv from 'dotenv';
 import db from './db';
 import routers from './routers'
 import cors from 'cors';
-
+import io from 'socket.io';
+import socketController from './socket';
 dotenv.config();
 
 const app = express();
 db().connect;
+const socketIo = io();
 
 app.use(helmet());
 app.use(logger("tiny"));
@@ -27,5 +29,14 @@ app.use('/', routers)
 app.listen(process.env.PORT, ()=>{
     console.log(`⛳ Express Server Listening at http://localhost:${process.env.PORT}`)
 });
+
+socketIo.use((socket, next)=> {
+    let token = socket.handshake.query.token;
+    if(isValid(token)) {
+        return next();
+    }
+    return next(new Error('login required'));
+});
+socketIo.on('connection', socketController)
 
 
