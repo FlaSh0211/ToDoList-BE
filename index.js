2fb9ae5d5929ed7fb5f2ebabbfff04da08c93985
FlaSh <nexus2493@gmail.com>
2020-12-14 02:25:02 +0900

    socket client server 연동 완료

--- index.js
+++ index.js
@@ -9,13 +9,12 @@ import dotenv from 'dotenv';
 import db from './db';
 import routers from './routers'
 import cors from 'cors';
-import io from 'socket.io';
-import socketController from './socket';
+import socketIO from 'socket.io';
+import { socketController } from './socket';
 dotenv.config();
 
 const app = express();
 db().connect;
-const socketIo = io();
 
 app.use(helmet());
 app.use(logger("tiny"));
@@ -26,17 +25,19 @@ app.use(passport.initialize());
 passportStrategy();
 app.use(cors())
 app.use('/', routers)
-app.listen(process.env.PORT, ()=>{
+const server = app.listen(process.env.PORT, ()=>{
     console.log(`⛳ Express Server Listening at http://localhost:${process.env.PORT}`)
 });
-
-socketIo.use((socket, next)=> {
-    let token = socket.handshake.query.token;
-    if(isValid(token)) {
-        return next();
-    }
-    return next(new Error('login required'));
+const io = socketIO(server, {
+    cors: {
+        origin: "http://localhost:3000",
+      }
 });
-socketIo.on('connection', socketController)
-
-
+// io.use((socket, next)=> {
+//     let token = socket.query.token;
+//     if(token) {
+//         return next();
+//     }
+//     return next(new Error('login required'));
+// });
+io.on('connection', socket => socketController(socket, io))
